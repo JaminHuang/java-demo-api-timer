bf864c04ad7c47f24e9bd855f5be6c368b6f9546
jamin <jamin.huang@pdabc.com>
2020-09-17 18:25:27 +0800

    处理启动项注解

--- src/main/java/com/demo/api/timer/App.java
+++ src/main/java/com/demo/api/timer/App.java
@@ -2,8 +2,16 @@ package com.demo.api.timer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableFeignClients
+@EnableEurekaClient
+@EnableTransactionManagement
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.demo"})
 public class App {
 
     public static void main(String[] args) {
