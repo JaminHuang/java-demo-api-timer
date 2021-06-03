5c9abc09dcc377aede0d136465fc46487a3405d6
JaminHuang <huanghzm@126.com>
2021-06-03 16:19:04 +0800

    添加testjob

--- src/main/java/com/demo/api/timer/job/TestJob.java
+++ src/main/java/com/demo/api/timer/job/TestJob.java
@@ -2,10 +2,13 @@ package com.demo.api.timer.job;
 
 import com.alibaba.fastjson.JSONObject;
 import com.demo.api.timer.business.UserBusiness;
+import com.demo.api.timer.business.impl.UserBusinessImpl;
 import com.demo.core.user.common.po.User;
 import com.demo.sdk.util.ExceptionUtils;
-import com.demo.timer.job.BaseJob;
-import com.xxl.job.core.handler.annotation.JobHandler;
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,23 +18,24 @@ import org.springframework.stereotype.Service;
  * @author jamin
  * @date 2020/9/17
  */
-@JobHandler("testJob")
 @Service
-public class TestJob extends BaseJob {
+public class TestJob {
+
+    private final static Logger logger = LoggerFactory.getLogger(UserBusinessImpl.class);
 
     @Autowired
     private UserBusiness userBusiness;
 
-    @Override
+    @XxlJob("testJob")
     protected void run(String param) {
 
         try {
 
-            logger.info("任务开启");
+            XxlJobHelper.log("任务开启");
 
             User user = userBusiness.get(1);
 
-            logger.info("[user] = {}", JSONObject.toJSONString(user));
+            XxlJobHelper.log("[user] = {}", JSONObject.toJSONString(user));
 
         }catch (Exception ex) {
             logger.error("测试任务失败执行异常，[error] = {}", ExceptionUtils.getExceptionMsg(ex));
